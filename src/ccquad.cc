c2c745bf007650a75d85b49839ad7400e14f403e
Yu Zhai <yuzhai@mail.huiligroup.org>
2022-06-20 16:17:01 +0800

    allocator

--- src/ccquad.cc
+++ src/ccquad.cc
@@ -5,7 +5,7 @@
 #include <vector>
 
 class CCIntegrator {
-private:
+public:
   std::vector<double> coss_;
   std::vector<double> sins_;
   // Storage
@@ -23,34 +23,46 @@ public:
     coss_.push_back(1.0);
     coss_.push_back(0.0);
     maxorder_ = 0;
-    order = 0;
+    allocate(order);
+    order_ = 0;
   }
   void set_order(size_t order) {
+    if (order > maxorder_) {
+      allocate(order);
+    }
     order_ = order;
     num_ = 1 << order >> 1;
     return;
   }
+  size_t idx(size_t r) {
+    if (r == 0)
+      return 0;
+    size_t b = (num_ >> 1);
+    while (!(r & 1)) {
+      r >>= 1;
+      b >>= 1;
+    }
+    r >>= 1;
+    r += b + 1;
+    return r;
+  }
   void allocate(size_t order) {
+    std::cerr << __LINE__ << "    " << order << std::endl;
     if (order <= maxorder_) {
       return;
     }
-    size_t n = pow(2, maxorder - 1) + 1;
-    sins_.reserve(n);
-    coss_.reserve(n);
-    for (size_t order = 1; order != maxorder; ++order) {
-      size_t maxnum = pow(2, order - 1);
+    size_t tmpord = order_;
+    set_order(order);
+    sins_.reserve(maxorder_);
+    coss_.reserve(maxorder_);
+    for (; maxorder_ != order; ++maxorder_) {
+      set_order(maxorder_);
+      maxnum_ = num_;
       double lc = coss_[0];
       double ls = sins_[0];
-      for (size_t i = 0; i != maxnum; ++i) {
-        size_t r = i + 1;
-        size_t b = (maxnum >> 1);
-        while (!(r & 1)) {
-          r >>= 1;
-          b >>= 1;
-        }
-        r >>= 1;
-        r += b + 1;
-        // std::cerr << r << std::endl;
+      for (size_t i = 0; i != maxnum_; ++i) {
+        size_t r = idx(i + 1);
+        std::cerr << r << std::endl;
         double rc = coss_[r];
         double rs = sins_[r];
         double c = lc * rc - ls * rs;
@@ -62,28 +74,29 @@ public:
         ls = rs;
       }
     }
+    set_order(tmpord);
+    return;
   }
 };
 
 int main() {
   std::cout << std::setprecision(8);
-  // 0 - pi
-  {
-    // Only consider [0,pi/2]
-    auto t0 = std::chrono::high_resolution_clock::now();
-    auto dt = t0 - t0;
-    // init
-    auto t1 = std::chrono::high_resolution_clock::now();
-    dt += t1 - t0;
-    std::cout << "Algo4: " << dt.count() << std::endl;
-  }
+  // Only consider [0,pi/2]
+  auto t0 = std::chrono::high_resolution_clock::now();
+  auto dt = t0 - t0;
+  size_t maxorder = 3;
+  CCIntegrator cc;
+  cc.set_order(maxorder);
+  // init
+  auto t1 = std::chrono::high_resolution_clock::now();
+  dt += t1 - t0;
+  std::cout << "Algo4: " << dt.count() << std::endl;
 
-  std::cout << coss_.size() << std::endl;
   // std::vector<int> ord{0, 8, 4, 2, 6, 1, 3, 5, 7}; // For test 0-8
   std::vector<int> ord{0, 4, 2, 1, 3}; // For test 0-8
   for (int i = 0; i <= pow(2, maxorder - 1); ++i) {
-    std::cout << coss_[i] << "  " << cos(M_PI / pow(2, maxorder) * ord[i])
-              << "  " << sins_[i] << "  "
+    std::cout << cc.coss_[i] << "  " << cos(M_PI / pow(2, maxorder) * ord[i])
+              << "  " << cc.sins_[i] << "  "
               << sin(M_PI / pow(2, maxorder) * ord[i]) << std::endl;
   }
 
