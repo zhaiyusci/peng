e8f74c75dffe970caf19fc36de36977a3696c8bf
Yu Zhai <yuzhai@mail.huiligroup.org>
2022-06-25 16:09:29 +0800

    singleton CG

--- src/cgquad.cc
+++ src/cgquad.cc
@@ -8,16 +8,18 @@
 #include "cgquad.hh"
 
 namespace dlt {
+CGIntegratorBackend *CGIntegratorBackend::instance_ = nullptr;
 CGIntegrator::CGIntegrator(size_t order) {
-  angles_.push_back(0.5); // pi/2
-  coss_.push_back(0.0);
-  maxorder_ = 0;
+  auto backend = CGIntegratorBackend::instance();
+  backend->angles_.push_back(0.5); // pi/2
+  backend->coss_.push_back(0.0);
+  backend->maxorder_ = 0;
   order_ = 0;
-  size_ = 1;
-  gap_ = 1.0;
-  allocate(order);
+  backend->size_ = 1;
+  backend->gap_ = 1.0;
+  backend->allocate(order);
 }
-void CGIntegrator::allocate(size_t order) {
+void CGIntegratorBackend::allocate(size_t order) {
   if (order <= maxorder_) {
     return;
   }
@@ -40,29 +42,34 @@ void CGIntegrator::allocate(size_t order) {
 std::tuple<double, double>
 CGIntegrator::integrate(std::function<double(double)> integrand, double a,
                         double b, double tol, size_t maxorder, bool negative) {
+  auto backend = CGIntegratorBackend::instance();
   // init
   double k0 = 0.5 * (b + a);
   double k1 = 0.5 * (b - a);
-  double res = (negative ? 1 : 0.5) * integrand(map_pm1(coss_[0], k0, k1));
+  double res = (negative ? 1 : 0.5) *
+               integrand(backend->map_pm1(backend->coss_[0], k0, k1));
   double oldint = 0.0;
   double newint = 0.0;
   double err = 0.0;
   for (size_t order = 1; order <= maxorder; ++order) {
     // std::cerr << "Order =    " << order << std::endl;
-    allocate(order);
+    backend->allocate(order);
     // std::cout << coss_.size() << std::endl;
     // std::cout << maxidx(order) << std::endl;
-    for (size_t i = maxidx(order - 1); i != maxidx(order); ++i) {
+    for (size_t i = CGIntegratorBackend::maxidx(order - 1);
+         i != CGIntegratorBackend::maxidx(order); ++i) {
       // std::cout << __FILE__ << __LINE__ << (map_pm1(coss_[i], k0, k1))
       // << std::endl;
-      res += integrand(map_pm1(coss_[i], k0, k1));
+      res += integrand(CGIntegratorBackend::map_pm1(backend->coss_[i], k0, k1));
     }
     if (negative) {
-      for (size_t i = maxidx(order - 1); i != maxidx(order); ++i) {
-        res += integrand(map_pm1(-coss_[i], k0, k1));
+      for (size_t i = CGIntegratorBackend::maxidx(order - 1);
+           i != CGIntegratorBackend::maxidx(order); ++i) {
+        res +=
+            integrand(CGIntegratorBackend::map_pm1(-backend->coss_[i], k0, k1));
       }
     }
-    integrand(map_pm1(coss_[0], k0, k1));
+    integrand(CGIntegratorBackend::map_pm1(backend->coss_[0], k0, k1));
     oldint = newint;
     newint = (negative ? 1 : 2) * res * M_PI / pow(3, order) * k1;
     err = fabs(oldint - newint);
@@ -77,8 +84,10 @@ CGIntegrator::integrate(std::function<double(double)> integrand, double a,
   }
   return std::make_tuple(newint, err);
 }
-double CGIntegrator::map_pm1(double x, double k0, double k1) {
+double CGIntegratorBackend::map_pm1(double x, double k0, double k1) {
   return k0 + k1 * x;
 }
-size_t CGIntegrator::maxidx(size_t order) { return (pow(3, order) + 1) / 2; }
+size_t CGIntegratorBackend::maxidx(size_t order) {
+  return (pow(3, order) + 1) / 2;
+}
 } // namespace dlt
--- src/cgquad.hh
+++ src/cgquad.hh
@@ -6,24 +6,41 @@
 #include <vector>
 
 namespace dlt {
+
 class CGIntegrator {
 public:
-  std::vector<double> angles_;
-  std::vector<double> coss_;
-  // Storage
-  size_t maxorder_;
-  double gap_;
-  size_t size_ = 1;
   // Status
   size_t order_;
 
 public:
   CGIntegrator() : CGIntegrator(1) {}
   CGIntegrator(size_t order);
-  void allocate(size_t order);
   std::tuple<double, double> integrate(std::function<double(double)> integrand,
                                        double a, double b, double tol,
                                        size_t maxorder, bool negative = true);
+};
+
+class CGIntegratorBackend {
+
+protected:
+  std::vector<double> angles_;
+  std::vector<double> coss_;
+  // Storage
+  size_t maxorder_;
+  double gap_;
+  size_t size_ = 1;
+  static CGIntegratorBackend *instance_;
+  CGIntegratorBackend(){};
+  friend CGIntegrator;
+
+public:
+  void allocate(size_t order);
+  static CGIntegratorBackend *instance() {
+    if (instance_ == nullptr) {
+      instance_ = new CGIntegratorBackend;
+    }
+    return instance_;
+  }
   static double map_pm1(double x, double k0, double k1);
   static size_t maxidx(size_t order);
 };
