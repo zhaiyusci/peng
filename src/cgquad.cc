5d54c7d076c6e02e07952b01487728d494ae2116
Yu Zhai <me@zhaiyusci.net>
2022-06-26 17:39:43 +0800

    n3

--- src/cgquad.cc
+++ src/cgquad.cc
@@ -9,119 +9,50 @@
 
 namespace dlt {
 
-size_t maxidx_symm(size_t order) { return (pow(3, order) + 1) / 2; }
-// This class used to iterate over the n^3 data...
-class CubicIter {
-private:
-  size_t border_;
-  size_t eorder_;
-  size_t symm_; // 0: only positive half
-                // 1: also negative half
-
-public:
-  CubicIter() = delete;
-  CubicIter(size_t eorder) : border_(0), eorder_(eorder), symm_(1) {}
-  CubicIter(size_t border, size_t eorder)
-      : border_(border), eorder_(eorder), symm_(1) {}
-  CubicIter(size_t border, size_t eorder, size_t symm)
-      : border_(border), eorder_(eorder), symm_(symm) {}
-  class iterator {
-  public:
-    CubicIter *ci_;
-    size_t order_;
-    size_t negative_;
-    size_t idx_;
-    size_t maxidx_;
-    size_t totidx_;
-
-  public:
-    iterator &operator++() {
-      ++totidx_;
-      ++idx_;
-      if (order_ == 0) {
-        idx_ = 0;
-        negative_ = 0;
-        order_ = 1;
-        maxidx_ = maxidx_symm(order_ - 1);
-        return *this;
-      }
-      if (idx_ != maxidx_) {
-        return *this;
-      }
-      idx_ = 0;
-      ++negative_;
-      if (ci_->symm_ == 1 && negative_ == 1)
-        totidx_ -= maxidx_;
-      if (negative_ != ci_->symm_ + 1) {
-        return *this;
-      }
-      idx_ = 0;
-      negative_ = 0;
-      ++order_;
-      maxidx_ = maxidx_symm(order_ - 1);
-      return *this;
-    }
-    int operator*() const { return totidx_ * (negative_ ? -1 : 1); }
-    bool operator==(iterator &rhs) const {
-      return order_ == rhs.order_ && negative_ == rhs.negative_ &&
-             idx_ == rhs.idx_;
-    }
-    bool operator!=(iterator &rhs) const { return !(operator==(rhs)); }
-    iterator(CubicIter *ci, size_t order, size_t negative, size_t idx)
-        : ci_(ci), order_(order), negative_(negative), idx_(idx) {
-      if (order == 0) {
-        totidx_ = 0;
-        maxidx_ = 1;
-      } else {
-        totidx_ = maxidx_symm(order - 1) + idx;
-        maxidx_ = maxidx_symm(order - 1);
-      }
-    }
-  };
-  iterator begin() { return iterator(this, border_, 0, 0); }
-  iterator end() { return iterator(this, eorder_ + 1, 0, 0); }
-};
-
 CGIntegratorBackend *CGIntegratorBackend::instance_ = nullptr;
-CGIntegrator::CGIntegrator(double a, double b) : a_(a), b_(b) {
+CGIntegrator::CGIntegrator(double a, double b, bool symm)
+    : a_(a), b_(b), symm_(symm) {
   k0_ = 0.5 * (b + a);
   k1_ = 0.5 * (b - a);
 }
 
-void CGIntegratorBackend::allocate(size_t order) {
-  if (order <= maxorder_) {
+void CGIntegratorBackend::allocate(size_t ordersize) {
+  if (ordersize <= ordersize_) {
     return;
   }
-  size_t num_ = maxidx_symm(order);
-  angles_.reserve(num_);
-  coss_.reserve(num_);
-  for (; maxorder_ != order; ++maxorder_) {
+  size_t num = (pow(3, ordersize - 1) + 1) / 2;
+  angles_.reserve(num);
+  coss_.reserve(num);
+  for (; ordersize_ != ordersize; ++ordersize_) {
+    CubicIter ci(1, ordersize_, true);
     gap_ /= 3.0;
-    for (size_t i = 1; i != maxidx_symm(maxorder_); ++i) {
+    for (auto &&i : ci) {
+      std::cout << __LINE__ << ' ' << i << '\n';
       angles_.push_back(angles_[i] - gap_);
       angles_.push_back(angles_[i] + gap_);
       coss_.push_back(cos(M_PI * (angles_[i] - gap_)));
       coss_.push_back(cos(M_PI * (angles_[i] + gap_)));
     }
+    std::cout << __LINE__ << ' ' << 0 << '\n';
     angles_.push_back(angles_[0] - gap_);
     coss_.push_back(cos(M_PI * (angles_[0] - gap_)));
   }
   return;
 }
 
-std::tuple<double, double> CGIntegrator::integrate(double tol, size_t maxorder,
-                                                   bool negative) {
+/*
+std::tuple<double, double> CGIntegrator::integrate(double tol,
+                                                   size_t maxordersize) {
   auto backend = CGIntegratorBackend::instance();
+  calculate_integrands(0);
   // init
-  double res = (negative ? 1 : 0.5) * integrand(map_pm1(backend->coss_[0]));
+  double res = (symm_ ? 0.5 : 1) * integrands_[0];
   double oldint = 0.0;
   double newint = 0.0;
   double err = 0.0;
-  for (size_t order = 1; order <= maxorder; ++order) {
-    // std::cerr << "Order =    " << order << std::endl;
-    backend->allocate(order);
-    // std::cout << coss_.size() << std::endl;
-    // std::cout << maxidx(order) << std::endl;
+  for (size_t ordersize = 1; ordersize <= maxordersize; ++ordersize) {
+    backend->allocate(ordersize);
+    CubicIter ci(ordersize - 1, ordersize, symm_);
     for (size_t i = CGIntegratorBackend::maxidx_symm(order - 1);
          i != CGIntegratorBackend::maxidx_symm(order); ++i) {
       // std::cout << __FILE__ << __LINE__ << (map_pm1(coss_[i], k0, k1))
@@ -149,6 +80,8 @@ std::tuple<double, double> CGIntegrator::integrate(double tol, size_t maxorder,
   }
   return std::make_tuple(newint, err);
 }
+*/
+
 double CGIntegrator::map_pm1(double x) { return k0_ + k1_ * x; }
 CGIntegratorBackend *CGIntegratorBackend::instance() {
   if (instance_ == nullptr) {
@@ -159,8 +92,48 @@ CGIntegratorBackend *CGIntegratorBackend::instance() {
 CGIntegratorBackend::CGIntegratorBackend() {
   angles_.push_back(0.5); // pi/2
   coss_.push_back(0.0);
-  maxorder_ = 0;
-  size_ = 1;
+  ordersize_ = 1;
   gap_ = 1.0;
 };
+
+double CGIntegratorBackend::coss(int i) {
+  return (i < 0 ? -1 : 1) * coss_[abs(i)];
+}
+
 } // namespace dlt
+
+int main() {
+  {
+    dlt::CubicIter ic(2, 3);
+    for (auto &&i : ic) {
+      std::cout << i << '\n';
+    }
+    std::cout << "=======" << std::endl;
+  }
+  {
+    dlt::CubicIter ic(2, 3, true);
+    for (auto &&i : ic) {
+      std::cout << i << '\n';
+    }
+    std::cout << "=======" << std::endl;
+  }
+  if (false) {
+    dlt::CubicIter ic(2, 3, true, false);
+    for (auto &&i : ic) {
+      std::cout << i << '\n';
+    }
+    std::cout << "=======" << std::endl;
+  }
+  {
+    dlt::CubicIter ic(2, 3, false, false);
+    for (auto &&i : ic) {
+      std::cout << i << '\n';
+    }
+    std::cout << "=======" << std::endl;
+  }
+  {
+    dlt::CubicIter ic(2, 3, false, false);
+    std::cout << *dlt::CubicIter::iterator(&ic, 2, 1, 1) << std::endl;
+  }
+  return 0;
+}
--- src/cgquad.hh
+++ src/cgquad.hh
@@ -7,18 +7,113 @@
 
 namespace dlt {
 
+// size_t half_stosize(size_t order) { return (pow(3, order) + 1) / 2; }
+// This class used to iterate over the n^3 data...
+class CubicIter {
+private:
+  size_t border_;
+  size_t eorder_; // eorder is not included
+  bool half_visit_;   // 0: only positive half
+                      // 1: also negative half
+  bool half_storage_; // in storage, only save the positive half
+
+public:
+  CubicIter() = delete;
+  CubicIter(size_t eorder)
+      : border_(0), eorder_(eorder), half_visit_(false), half_storage_(true) {}
+  CubicIter(size_t border, size_t eorder, size_t half_visit = false,
+            bool half_storage = true)
+      : border_(border), eorder_(eorder), half_visit_(half_visit),
+        half_storage_(half_storage) {
+    if (half_visit_ && !half_storage_) {
+      throw std::runtime_error("You cannot only visit the positive half of the "
+                               "elements in the full storage.");
+    }
+  }
+  class iterator {
+  protected:
+    CubicIter *ci_;
+    size_t order_;
+    size_t negative_;
+    size_t idx_;
+    size_t maxidx_;
+    size_t totidx_;
+
+  public:
+    iterator &operator++() {
+      ++totidx_;
+      ++idx_;
+      if (order_ == 0) {
+        idx_ = 0;
+        negative_ = 0;
+        order_ = 1;
+        maxidx_ = 1;
+        return *this;
+      }
+      if (idx_ != maxidx_) {
+        return *this;
+      }
+      idx_ = 0;
+      ++negative_;
+      if ((!ci_->half_visit_) && negative_ == 1 && ci_->half_storage_)
+        totidx_ -= maxidx_;
+      if (negative_ != (ci_->half_visit_ ? 0 : 1) + 1) {
+        return *this;
+      }
+      idx_ = 0;
+      negative_ = 0;
+      ++order_;
+      maxidx_ = pow(3, order_ - 1);
+      return *this;
+    }
+    size_t order() { return order_; }
+    bool negative() { return negative_ == 1; }
+    size_t idx() { return idx_; }
+    int operator*() const {
+      if (ci_->half_storage_) {
+        return totidx_ * (negative_ ? -1 : 1);
+      } else {
+        return totidx_;
+      }
+    }
+    bool operator==(iterator &rhs) const {
+      return order_ == rhs.order_ && negative_ == rhs.negative_ &&
+             idx_ == rhs.idx_;
+    }
+    bool operator!=(iterator &rhs) const { return !(operator==(rhs)); }
+
+    iterator(CubicIter *ci, size_t order, size_t negative, size_t idx)
+        : ci_(ci), order_(order), negative_(negative), idx_(idx) {
+      if (order == 0) {
+        totidx_ = 0;
+        maxidx_ = 1;
+      } else {
+        maxidx_ = pow(3, order - 1);
+        if (ci_->half_storage_) {
+          totidx_ = (pow(3, order - 1) + 1) / 2 + idx;
+        } else {
+          totidx_ = pow(3, order - 1) + negative * maxidx_ + idx;
+        }
+      }
+    }
+  };
+  iterator begin() { return iterator(this, border_, 0, 0); }
+  iterator end() { return iterator(this, eorder_, 0, 0); }
+};
+
 class CGIntegrator {
 protected:
   double a_;
   double b_;
   double k0_;
   double k1_;
-  bool is_symm_;
+  bool symm_;
+  std::vector<double> integrands_;
 
 public:
-  CGIntegrator(double a, double b);
-  std::tuple<double, double> integrate(double tol, size_t maxorder,
-                                       bool negative = true);
+  CGIntegrator(double a, double b, bool symm);
+  std::tuple<double, double> integrate(double tol, size_t ordersize);
+  // virtual void calculate_integrands(size_t maxordersize);
   double map_pm1(double x);
 };
 
@@ -28,24 +123,16 @@ protected:
   std::vector<double> angles_;
   std::vector<double> coss_;
   // Storage
-  size_t maxorder_;
-  double gap_;
-  size_t size_ = 1;
+  size_t ordersize_; // ordersize is not included following C++ convention
+  double gap_;       // keep track of the gap between neighbouring two pivots
   static CGIntegratorBackend *instance_;
   CGIntegratorBackend();
   friend CGIntegrator;
 
 public:
-  void allocate(size_t order);
   static CGIntegratorBackend *instance();
-};
-
-class N3Allocator {
-protected:
-  std::vector<double> vals_;
-  size_t maxorder_;
-
-public:
   void allocate(size_t order);
+  double coss(int i);
 };
+
 } // namespace dlt
